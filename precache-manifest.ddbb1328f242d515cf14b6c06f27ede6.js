ec6b0bf6e79c95538935a295c2f918f84a181c71
shivanipyarsabadi <shivani.pyarsabadi@gmail.com>
2020-09-15 11:12:27 +0200

    Updates

---
+++ precache-manifest.ddbb1328f242d515cf14b6c06f27ede6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a225a5851c817f1ecde882c0a2e1c046",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "2e2b9e7832e2d1059ae6",
+    "url": "/my-portfolio/static/css/main.d60da7ec.chunk.css"
+  },
+  {
+    "revision": "d1886071fb6fcfb68ff0",
+    "url": "/my-portfolio/static/js/2.480de5ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.480de5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2b9e7832e2d1059ae6",
+    "url": "/my-portfolio/static/js/main.ca442e29.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "9d3cf6447c5c98fe78563cf52b539b36",
+    "url": "/my-portfolio/static/media/05.9d3cf644.jpg"
+  },
+  {
+    "revision": "c77ac42448c6f64404a8aac076541ac4",
+    "url": "/my-portfolio/static/media/Pyarsabadi_Shivani_Resume.c77ac424.pdf"
+  }
+]);
